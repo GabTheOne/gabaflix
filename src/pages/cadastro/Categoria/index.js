ec4d0c647671001f605dc63d97a0f9f6f0fc4f98
Gabriel <gabriel_g_alves@hotmail.com>
2020-08-02 18:15:59 +0100

    Melhorias na página de categorias

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,21 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import PageDefault from "../../../components/PageDefault";
 import { Link } from "react-router-dom";
 
 function Categoria() {
+  const [categories, setCategories] = useState([]);
+
+  const [category, setCategory] = useState({
+    name: "Gaba",
+    description: "",
+    color: "#000",
+  });
+
+  function setValue(key, value) {  
+    setCategory({
+      ...category,
+      [key]: value,
+    });
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria {category.name}</h1>
 
-      <form>
-        <label>
-          Nome da Categoria:
-          <input type="text" />
-        </label>
+      <form
+        onSubmit={function handleSubmit(oEvent) {
+          oEvent.preventDefault();
+          setCategories([...categories, category]);
+        }}
+      >
+        <div>
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+              value={category.name}
+              onChange={(oEvent) => {
+                setValue("name", oEvent.target.value);
+              }}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Categoria:
+            <textarea
+              type="text"
+              value={category.description}
+              onChange={(oEvent) => {
+                setValue("description", oEvent.target.value);
+              }}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Cor:
+            <input
+              type="color"
+              value={category.color}
+              onChange={(oEvent) => {
+                setValue("color", oEvent.target.value);
+              }}
+            />
+          </label>
+        </div>
 
         <button>Cadastrar</button>
       </form>
 
+      <ul>
+        {categories.map((cat, idx) => {
+          return <li key={`${cat}${idx}`}>{cat}</li>;
+        })}
+      </ul>
       <Link to="/">Ir para home</Link>
     </PageDefault>
   );
