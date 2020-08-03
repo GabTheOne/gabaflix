f611b00b0b523980b0fd25357452d313a0036223
Gabriel <gabriel_g_alves@hotmail.com>
2020-08-03 23:21:34 +0100

    Criação de formulario

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 import PageDefault from "../../../components/PageDefault";
 import { Link } from "react-router-dom";
+import FormField from "../../../components/FormField";
 
 function Categoria() {
   const [categories, setCategories] = useState([]);
 
-  const [category, setCategory] = useState({
-    name: "Gaba",
+  const initialCategory = {
+    name: "",
     description: "",
     color: "#000",
-  });
+  };
 
-  function setValue(key, value) {  
+  const [category, setCategory] = useState(initialCategory);
+
+  function setValue(oEvent) {  
     setCategory({
       ...category,
-      [key]: value,
+      [oEvent.target.getAttribute("name")]: oEvent.target.value,
     });
   }
 
@@ -26,52 +29,42 @@ function Categoria() {
         onSubmit={function handleSubmit(oEvent) {
           oEvent.preventDefault();
           setCategories([...categories, category]);
+          setCategory(initialCategory);
         }}
       >
-        <div>
-          <label>
-            Nome da Categoria:
-            <input
-              type="text"
-              value={category.name}
-              onChange={(oEvent) => {
-                setValue("name", oEvent.target.value);
-              }}
-            />
-          </label>
-        </div>
+
+        <FormField  
+          label="Nome da Categoria"
+          name="name"
+          type="text"
+          value={category.name} 
+          onChange={setValue} />
         <div>
           <label>
             Categoria:
             <textarea
               type="text"
+              name="description"
               value={category.description}
-              onChange={(oEvent) => {
-                setValue("description", oEvent.target.value);
-              }}
+              onChange={setValue}
             />
           </label>
         </div>
 
-        <div>
-          <label>
-            Cor:
-            <input
-              type="color"
-              value={category.color}
-              onChange={(oEvent) => {
-                setValue("color", oEvent.target.value);
-              }}
-            />
-          </label>
-        </div>
+
+        <FormField  
+          label="Cor"
+          name="color"
+          type="color"
+          value={category.color} 
+          onChange={setValue} />
 
         <button>Cadastrar</button>
       </form>
 
       <ul>
         {categories.map((cat, idx) => {
-          return <li key={`${cat}${idx}`}>{cat}</li>;
+          return <li key={`${cat.name}${idx}`}>{cat.name}</li>;
         })}
       </ul>
       <Link to="/">Ir para home</Link>
